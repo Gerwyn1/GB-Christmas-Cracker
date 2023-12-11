c58adfa80739e2ac444cd6e377a9d11acf69d3ba
Gerwyn <gerwynsim@hotmail.com>
2023-12-11 12:23:49 +0800

    add partial pull effect

--- src/components/CrackerPicked.jsx
+++ src/components/CrackerPicked.jsx
@@ -1,15 +1,25 @@
 /* eslint-disable react/prop-types */
 
-// import pop_b from "/Images/SVG/pop_b_bg.svg";
-
 // eslint-disable-next-line no-unused-vars
 
+// setStage("cracker opened")
+
+import { useState } from "react";
+
 const CrackerPicked = ({ setStage, color }) => {
+  const [scaleX, setSetScaleX] = useState(1);
+  const [scaleY, setSetScaleY] = useState(1);
+  const [crackerTop, setCrackerTop] = useState("23.2%");
+  const [crackerBottom, setCrackerBottom] = useState("50%");
+  const [pull, setPull] = useState(0);
+  console.log(pull);
+
   return (
     <div
       style={{
         position: "relative",
         height: "100vh",
+        overflow: "hidden",
       }}
     >
       <img
@@ -20,34 +30,53 @@ const CrackerPicked = ({ setStage, color }) => {
           width: "100%",
         }}
       />
-      <div style={{ position: "relative",  transform:"translate(-2.5%, -105%) rotate(-22.5deg)", height:'100%' }}>
+      <div
+        style={{
+          position: "relative",
+          transform: `translate(-2.5%, -105%) rotate(-22.5deg) scale(${
+            pull === 6 ? 1 : scaleX
+          }, ${pull === 6 ? 1 : scaleY})`,
+          height: "100%",
+        }}
+        onClick={() => {
+          setSetScaleX((x) => (x -= 0.05));
+          setSetScaleY((y) => (y += 0.05));
+          setCrackerTop((position) => position.replace("%", "") - 0.1 + "%");
+          setCrackerBottom(
+            (position) => Number(position.replace("%", "")) + 0.1 + "%"
+          );
+          setPull((pull) => {
+            if (pull >= 6) {
+              pull = 0;
+              return pull;
+            }
+            pull += 1;
+            return pull;
+          });
+        }}
+      >
         <img
           src={`/public/GB_SPLIT/pop_top_${color}.webp`}
           alt="cracker top"
           style={{
-            top:'27.1%',
-            left:'37.5%',
-            // transform:('translate(-50%, -50%)'),
+            top: pull >= 6 ? "23.2%" : crackerTop,
+            left: "37.5%",
             position: "absolute",
             width: "20%",
-            height: "30%",
+            height: "35%",
             cursor: "pointer",
-            // transform:('translateY(-400%)')
           }}
         />
         <img
           src={`/public/GB_SPLIT/pop_btm_${color}.webp`}
           alt="cracker bottom"
           style={{
-            top:'50%',
-            left:'37.5%',
-            // transform:('translate(-50%, -50%)'),
-            position:'absolute',
+            top: pull >= 6 ? "50%" : crackerBottom,
+            left: "37.5%",
+            position: "absolute",
             width: "20%",
-            height: "30%",
+            height: "35%",
             cursor: "pointer",
-            // transform:('translate(-100%, -324%)'),
-            // transform:('translateX(-10%)')
           }}
         />
       </div>
@@ -57,5 +86,4 @@ const CrackerPicked = ({ setStage, color }) => {
 
 export default CrackerPicked;
 
-// onClick={() => setStage("cracker opened")}
 // animation: "crackerAnimation 2s infinite"
